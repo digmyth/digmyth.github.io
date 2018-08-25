e44e352cc76360909fd0117adc6c73d24184c01d
digmyth <wxqcto@163.com>
2018-08-25 09:37:23 +0800

    Site updated: 2018-08-25 09:37:23

--- mcommon.js
+++ mcommon.js
@@ -1,4 +1,4 @@
-function decryptAES1() {
+function decryptAES() {
     var pass = String(document.getElementById("pass").value);
     try {
         var content = CryptoJS.AES.decrypt(document.getElementById("encrypt-blog").innerHTML.trim(), pass);
@@ -7,7 +7,7 @@ function decryptAES1() {
         console.log(content);
         content = unescape(content);
         if (content == '') {
-            alert("大爷常来玩");
+            alert("密码错误！！");
         } else {
             document.getElementById("encrypt-blog").style.display    = "inline";
             document.getElementById("encrypt-blog").innerHTML        = content;
@@ -20,54 +20,10 @@ function decryptAES1() {
             }
         }
     } catch (e) {
-        alert("大爷常来玩");
+        alert("密码错误！！");
         console.log(e);
     }
 }
-
-
-
-function decryptAES() {
-
- var pass = String(document.getElementById("pass").value);
-    try {
-        var content = CryptoJS.AES.decrypt(document.getElementById("encrypt-blog").innerHTML.trim(), pass);
-        content = content.toString(CryptoJS.enc.Utf8);
-        content = decodeBase64(content);
-        console.log(content);
-        content = unescape(content);
-        if (content == '') {
-swal({
-  position: 'top-end',
-  type: 'success',
-  title: '常来玩',
-  showConfirmButton: false,
-  timer: 1500
-})
-//            alert("大爷常来玩");
-
-        } else {
-            document.getElementById("encrypt-blog").style.display    = "inline";
-            document.getElementById("encrypt-blog").innerHTML        = content;
-            document.getElementById("encrypt-message").style.display = "none";
-
-            document.getElementById("security").style.display        = "none";
-
-            if (document.getElementById("toc-div")) {
-                document.getElementById("toc-div").style.display     = "inline";
-            }
-        }
-    } catch (e) {
-  //      alert("大爷常来玩");
-  //      console.log(e);
-    }
-
-
-
-}
-
-
-
 
 function htmlDecode (str) {
     var s = "";
